95ff1947027f6eac14cf20c0e807d83d29d8eb82
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-05-30 18:44:26 +0530

    Update SignupForm.jsx

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 import { useFormik } from 'formik'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import googleIcon from '../assets/gmail.png';
 export default function SignupForm() {
-
-
-
-
-  
     const formik = useFormik({
       initialValues : {
         email:'',
@@ -18,8 +14,8 @@ export default function SignupForm() {
         const errors = {};
         if (!values.password) {
           errors.password = 'Required';
-        } else if (values.password.length > 15) {
-          errors.password = 'Must be 15 characters';
+        } else if (values.password.length < 6) {
+          errors.password = 'Must be 6 characters';
         }
       
         if (!values.email) {
@@ -46,19 +42,19 @@ export default function SignupForm() {
              <div className="inputField">
              <label htmlFor="email">Email</label>
               <input className='formInput' type="email" id="email"  name="email" onChange={formik.handleChange} value={formik.values.email} placeholder='Enter your email' onBlur={formik.handleBlur}/>
-              {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+              {formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
               </div>
               <div className="inputField">
               <label htmlFor="password">password</label>
               <input className='formInput' type="password" id="password" name="password" onChange={formik.handleChange} value={formik.values.password} placeholder='Enter your password' onBlur={formik.handleBlur}/>
-              {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null}
+              {formik.touched.password && formik.errors.password ? <div className='error'>{formik.errors.password}</div> : null}
               </div>
               
               <button type="submit">Login</button>
           </form>
 
           <div className="separate-or"><div className="line"></div> <span>or </span> <div className="line"></div></div>
-          <button type="button" className="colorlight">Sign up with <FontAwesomeIcon icon={faGoogle}/></button>
+          <button type="button" className="colorlight">Sign up with <img src={googleIcon} alt="" srcset=""  style={ {height:"14px"}}/></button>
 
        </div>
     </div>
